35764c5248ae575bbc0a5204630b58ceb1320000
Malki Yasara <258165632+malkiyasara@users.noreply.github.com>
2026-09-17 21:56:45 +0530

    feat: add authentication, user, profile, dashboard, and notification services

---
+++ client/src/services/user.service.ts
@@ -0,0 +1,16 @@
+import api from "./api";
+
+export const getUsers = async () => {
+  const response = await api.get("/users");
+  return response.data;
+};
+
+export const getUserById = async (id) => {
+  const response = await api.get(`/users/${id}`);
+  return response.data;
+};
+
+export const deleteUser = async (id) => {
+  const response = await api.delete(`/users/${id}`);
+  return response.data;
+};
